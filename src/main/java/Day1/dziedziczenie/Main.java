e07ced0dcdd3acca5a374a2dfabc4eabf26b0559
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-10-02 20:54:23 +0200

    Day2

--- src/main/java/Day1/dziedziczenie/Main.java
+++ src/main/java/Day1/dziedziczenie/Main.java
@@ -3,6 +3,7 @@ package Day1.dziedziczenie;
 public class Main {
     public static void main(String[] args) {
 
-        Hippo hippo = new Hippo("some Hippo",15);
+        Hippo hippo = new Hippo("some Hippo", 15);
     }
 }
+
