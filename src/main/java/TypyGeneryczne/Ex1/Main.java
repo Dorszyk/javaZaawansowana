4ed7eab90feb7226e2b62ba5dfbfec4ab9c2661d
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2021-10-15 20:17:10 +0200

    Ex1typyGeneryczne

--- src/main/java/TypyGeneryczne/Ex1/Main.java
+++ src/main/java/TypyGeneryczne/Ex1/Main.java
@@ -1,4 +1,9 @@
 package TypyGeneryczne.Ex1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Pair<Integer,String>pair = new Pair<>(23,"JavaAdvanced");
+        System.out.println(pair);
+    }
 }
